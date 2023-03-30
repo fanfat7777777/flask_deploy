9bdaa41ae89f82bd6c63ec145b1936e94474bf13
fanfat7777777 <93639218+fanfat7777777@users.noreply.github.com>
2023-03-30 08:27:46 +0300

    Merge copy => master
    Co-authored-by: fanfat <fanfat@fanfat.ru>

--- blog/forms/article.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, validators, SubmitField, TextAreaField, SelectMultipleField
-
-
-class CreateArticleForm(FlaskForm):
-    title = StringField('Title', [validators.DataRequired()])
-    text = TextAreaField('Text', [validators.DataRequired()])
-    tags = SelectMultipleField('Tags', coerce=int)
-    submit = SubmitField('Create')
